4103a0bc9dcc804d071b302a697b9ef6e1d11f55
kheizaran-nazari-khakeshoori <kheizarannazarikhakeshoori@gmail.com>
2025-09-07 16:48:26 +0200

    define base MazeEnv class structure

--- maze_solver/env.py
+++ maze_solver/env.py
@@ -3,3 +3,12 @@
 import gymnasium as gym
 import numpy as np
 
+
+class MazeEnv(gym.Env):
+    """Custom Gridworld maze environment."""
+
+    metadata = {"render_modes": ["human"]}
+
+    def __init__(self):
+        super().__init__()
+
