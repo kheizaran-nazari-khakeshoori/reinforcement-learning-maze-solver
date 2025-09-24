a885e26f4a78f7491a613e7d7457afd2226482eb
kheizaran-nazari-khakeshoori <kheizarannazarikhakeshoori@gmail.com>
2025-09-24 17:34:38 +0200

    define BaseAgent with alpha gamma

--- maze_solver/agents.py
+++ maze_solver/agents.py
@@ -2,6 +2,7 @@
 import numpy as np
 class BaseAgent:
     """Base."""
-    def __init__(self, n_states, n_actions):
+    def __init__(self, n_states, n_actions, alpha=0.1, gamma=0.99):
         self.n_states=n_states; self.n_actions=n_actions
+        self.alpha=alpha; self.gamma=gamma
         self.q_table=np.zeros((n_states,n_actions))
