b98d2ecdf1e4cf87b7a1a7a2479e06f650200b3b
kheizaran-nazari-khakeshoori <kheizarannazarikhakeshoori@gmail.com>
2025-09-25 17:35:14 +0200

    create QLearningAgent

--- maze_solver/agents.py
+++ maze_solver/agents.py
@@ -14,3 +14,9 @@ class BaseAgent:
         if np.random.rand()<epsilon:
             return self.random_action()
         return self.greedy_action(state)
+
+class QLearningAgent(BaseAgent):
+    """Q-Learning."""
+    def update(self, s,a,r,ns,done):
+        best=0.0 if done else float(np.max(self.q_table[ns]))
+        self.q_table[s,a]+=self.alpha*(r+self.gamma*best-self.q_table[s,a])
