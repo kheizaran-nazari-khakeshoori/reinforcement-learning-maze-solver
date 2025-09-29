e4bf2463bbeb342d7bbf86533d7ba383b265ea68
kheizaran-nazari-khakeshoori <kheizarannazarikhakeshoori@gmail.com>
2025-09-29 17:36:55 +0200

    add moving average

--- plot.py
+++ plot.py
@@ -1,4 +1,10 @@
 """Plot."""
+import numpy as np
 import matplotlib.pyplot as plt
+def moving_average(data, w=10):
+    return np.convolve(data, np.ones(w)/w, mode="valid")
 def plot_rewards(rewards):
-    plt.figure(); plt.plot(rewards); plt.savefig("curve.png")
+    plt.figure(); plt.plot(rewards, alpha=0.3)
+    ma=moving_average(rewards, 10)
+    plt.plot(range(9, len(rewards)), ma)
+    plt.savefig("curve.png")
