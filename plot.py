11068149504a467f0b702ebbb08dafc05d56861a
kheizaran-nazari-khakeshoori <kheizarannazarikhakeshoori@gmail.com>
2025-09-29 17:36:55 +0200

    add save with legend

--- plot.py
+++ plot.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 def moving_average(data, w=10):
     return np.convolve(data, np.ones(w)/w, mode="valid")
-def plot_rewards(rewards):
-    plt.figure(); plt.plot(rewards, alpha=0.3)
+def plot_rewards(rewards, save="curve.png"):
+    plt.figure(); plt.plot(rewards, alpha=0.3, label="raw")
     ma=moving_average(rewards, 10)
-    plt.plot(range(9, len(rewards)), ma)
-    plt.savefig("curve.png")
+    plt.plot(range(9, len(rewards)), ma, label="ma")
+    plt.legend(); plt.tight_layout(); plt.savefig(save)
